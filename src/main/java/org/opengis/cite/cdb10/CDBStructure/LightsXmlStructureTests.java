051bb663f3a4d7ca6fd3c4ffea18fb0ceb3bb7a9
martinechtner <martin.echtner@gmail.com>
2016-09-02 09:54:44 -0600

    Refactored and DRYed code

--- src/main/java/org/opengis/cite/cdb10/CDBStructure/LightsXmlStructureTests.java
+++ src/main/java/org/opengis/cite/cdb10/CDBStructure/LightsXmlStructureTests.java
@@ -2,6 +2,7 @@ package org.opengis.cite.cdb10.CDBStructure;
 
 import org.opengis.cite.cdb10.CommonFixture;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -13,57 +14,60 @@ import javax.xml.xpath.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Objects;
 
 /**
  * Created by martin on 2016-09-01.
  */
 public class LightsXmlStructureTests extends CommonFixture {
+
+    @Test
     public void verifyLightsXmlFileExist() {
         Assert.assertTrue(Files.exists(Paths.get(path, "Metadata", "Lights.xml")), "Metadata should contain Lights.xml file.");
     }
 
+    @Test
     public void verifyLightsXmlHasUniqueCodes() {
-        try {
-            Document doc;
-            doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(String.valueOf(Paths.get(path, "Metadata", "Lights.xml")));
-            XPath xPath = XPathFactory.newInstance().newXPath();
-            XPathExpression exp = xPath.compile("//Light");
-            NodeList nl = (NodeList) exp.evaluate(doc, XPathConstants.NODESET);
+        NodeList nodeList = getNodeList();
 
-            for (int i = 0; i < nl.getLength(); i++) {
-                Node currentItem = nl.item(i);
-                String key = currentItem.getAttributes().getNamedItem("code").getNodeValue();
+        ArrayList<String> codes = new ArrayList<>();
 
-                int codeCount = 0;
-                for (int j = 0; j < nl.getLength(); j++) {
-                    if (Objects.equals(key, nl.item(j).getAttributes().getNamedItem("code").getNodeValue())) {
-                        codeCount++;
-                    }
-                }
-                Assert.assertEquals(codeCount, 1, "Lights.xml element Light should have a unique codes.");
-            }
-        } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException ex) {
-            ex.printStackTrace();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node currentItem = nodeList.item(i);
+            codes.add(currentItem.getAttributes().getNamedItem("code").getNodeValue());
+        }
+
+        for (String code : codes) {
+            Assert.assertEquals(Collections.frequency(codes, code), 1,
+                    String.format("Lights.xml element Light should have unique codes. Code %s is not unique.", code));
+        }
+
+    }
+
+    @Test
+    public void verifyLightsXmlCodesAreWithinRange() {
+        NodeList nodeList = getNodeList();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node currentItem = nodeList.item(i);
+            int key = Integer.parseInt(currentItem.getAttributes().getNamedItem("code").getNodeValue());
+
+            Assert.assertTrue((key >= 0) && (key <= 9999), "Lights.xml element Light should have a code from 0 - 9999 inclusive.");
         }
     }
 
-    public void verifyLightsXmlCodesAreWithinRange() {
+    private NodeList getNodeList() {
         try {
             Document doc;
             doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(String.valueOf(Paths.get(path, "Metadata", "Lights.xml")));
             XPath xPath = XPathFactory.newInstance().newXPath();
             XPathExpression exp = xPath.compile("//Light");
-            NodeList nl = (NodeList) exp.evaluate(doc, XPathConstants.NODESET);
-
-            for (int i = 0; i < nl.getLength(); i++) {
-                Node currentItem = nl.item(i);
-                int key = Integer.parseInt(currentItem.getAttributes().getNamedItem("code").getNodeValue());
-
-                Assert.assertTrue((key >= 0) && (key <= 9999), "Lights.xml element Light should have a code from 0 - 9999 inclusive.");
-            }
+            return (NodeList) exp.evaluate(doc, XPathConstants.NODESET);
         } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException ex) {
             ex.printStackTrace();
         }
+        return null;
     }
 }
