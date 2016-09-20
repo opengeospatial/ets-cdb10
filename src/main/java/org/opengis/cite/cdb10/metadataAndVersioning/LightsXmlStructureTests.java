4d34b2a32f56727939a9a8a4d322ca261fdbb102
martinechtner <martin.echtner@gmail.com>
2016-09-20 16:03:41 -0600

    refactor to use an object

--- src/main/java/org/opengis/cite/cdb10/metadataAndVersioning/LightsXmlStructureTests.java
+++ src/main/java/org/opengis/cite/cdb10/metadataAndVersioning/LightsXmlStructureTests.java
@@ -23,48 +23,21 @@ public class LightsXmlStructureTests extends CommonFixture {
 
     @Test
     public void verifyLightsXmlFileExists() {
-        Assert.assertTrue(Files.exists(Paths.get(path, "Metadata", "Lights.xml")), "Metadata directory should contain Lights.xml file.");
+        new LightsXml(path);
     }
 
     @Test
     public void verifyLightsXmlFileAgainstSchema() throws IOException, SAXException {
-        File xmlFile = Paths.get(path, "Metadata", "Lights.xml").toFile();
-        File xsdFile = Paths.get(path, "Metadata", "Schema", "Lights.xsd").toFile();
-
-        SchemaValidatorErrorHandler errorHandler = XMLUtils.validateXmlFileIsValid(xmlFile, xsdFile);
-
-        if (!errorHandler.noErrors()) {
-            Assert.fail(xmlFile.getName() + " does not contain valid XML. Errors: " + errorHandler.getMessages());
-        }
+        new LightsXml(path).verifyXmlAgainstSchema();
     }
 
     @Test
     public void verifyLightsXmlHasUniqueCodes() {
-        NodeList nodeList = XMLUtils.getNodeList("//Light", Paths.get(path, "Metadata", "Lights.xml"));
-
-        ArrayList<String> codes = new ArrayList<>();
-
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node currentItem = nodeList.item(i);
-            codes.add(currentItem.getAttributes().getNamedItem("code").getNodeValue());
-        }
-
-        for (String code : codes) {
-            Assert.assertEquals(Collections.frequency(codes, code), 1,
-                    String.format("Lights.xml element Light should have unique codes. Code '%s' is not unique.", code));
-        }
+        new LightsXml(path).verifyHasUniqueCodes();
     }
 
     @Test
     public void verifyLightsXmlCodesAreWithinRange() {
-        NodeList nodeList = XMLUtils.getNodeList("//Light", Paths.get(path, "Metadata", "Lights.xml"));
-
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node currentItem = nodeList.item(i);
-            int key = Integer.parseInt(currentItem.getAttributes().getNamedItem("code").getNodeValue());
-
-            Assert.assertTrue((key >= 0) && (key <= 9999), "Lights.xml element Light should have a code from 0 - 9999 inclusive.");
-        }
+        new LightsXml(path).verifyCodesAreWithinRange();
     }
-
 }
