287a10e6568b0cec07cddf7c0889a270a016e4a3
martinechtner <martin.echtner@gmail.com>
2016-09-20 16:23:40 -0600

    refactor to an object

---
+++ src/main/java/org/opengis/cite/cdb10/metadataAndVersioning/ModelComponentsXml.java
@@ -0,0 +1,10 @@
+package org.opengis.cite.cdb10.metadataAndVersioning;
+
+/**
+ * Created by martin on 2016-09-20.
+ */
+public class ModelComponentsXml extends MetadataXmlFile {
+    public ModelComponentsXml(String path) {
+        super(path, "Model_Components.xml", "Model_Components.xsd");
+    }
+}
--- src/main/java/org/opengis/cite/cdb10/metadataAndVersioning/ModelComponentsXmlStructureTests.java
+++ src/main/java/org/opengis/cite/cdb10/metadataAndVersioning/ModelComponentsXmlStructureTests.java
@@ -1,16 +1,10 @@
 package org.opengis.cite.cdb10.metadataAndVersioning;
 
 import org.opengis.cite.cdb10.CommonFixture;
-import org.opengis.cite.cdb10.util.SchemaValidatorErrorHandler;
-import org.opengis.cite.cdb10.util.XMLUtils;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.xml.sax.SAXException;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 /**
  * Created by martin on 2016-09-02.
@@ -19,19 +13,11 @@ public class ModelComponentsXmlStructureTests extends CommonFixture {
 
     @Test
     public void verifyModelComponentsXmlFileExists() {
-        Assert.assertTrue(Files.exists(Paths.get(path, "Metadata", "Model_Components.xml")),
-                "Metadata directory should contain Model_Components.xml file.");
+        new ModelComponentsXml(path);
     }
 
     @Test
     public void verifyModelComponentsXmlAgainstSchema() throws IOException, SAXException {
-        File xmlFile = Paths.get(path, "Metadata", "Model_Components.xml").toFile();
-        File xsdFile = Paths.get(path, "Metadata", "Schema", "Model_Components.xsd").toFile();
-
-        SchemaValidatorErrorHandler errorHandler = XMLUtils.validateXmlFileIsValid(xmlFile, xsdFile);
-
-        if (!errorHandler.noErrors()) {
-            Assert.fail(xmlFile.getName() + " does not contain valid XML. Errors: " + errorHandler.getMessages());
-        }
+        new ModelComponentsXml(path).verifyXmlAgainstSchema();
     }
 }
