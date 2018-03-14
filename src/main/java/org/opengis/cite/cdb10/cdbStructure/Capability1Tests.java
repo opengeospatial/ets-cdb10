a3d8f8b85fb5c3979b2760c7d331e62420d457e5
James Badger <james@jamesbadger.ca>
2018-03-14 14:27:57 -0600

    Only run conformance level 1 if included in ics

--- src/main/java/org/opengis/cite/cdb10/cdbStructure/Capability1Tests.java
+++ src/main/java/org/opengis/cite/cdb10/cdbStructure/Capability1Tests.java
@@ -2,7 +2,10 @@ package org.opengis.cite.cdb10.cdbStructure;
 
 import org.apache.commons.lang3.StringUtils;
 import org.opengis.cite.cdb10.CommonFixture;
+import org.opengis.cite.cdb10.SuiteAttribute;
 import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -20,6 +23,27 @@ import java.util.Collections;
 public class Capability1Tests extends CommonFixture {
 
 	public Capability1Tests() {
+	}
+
+	/**
+	 * Obtains the test subject from the ISuite context. The suite attribute
+	 * {@link org.opengis.cite.cdb10.SuiteAttribute#TEST_SUBJECT} should evaluate to
+	 * a DOM Document node.
+	 *
+	 * @param testContext
+	 *            The test (group) context.
+	 */
+	@Override
+	@BeforeClass
+	public void obtainTestSubject(ITestContext testContext) {
+		Object obj = testContext.getSuite().getAttribute(SuiteAttribute.LEVELS.getName());
+		System.out.println(obj);
+		if ((null != obj)) {
+			ArrayList<Integer> levels = new ArrayList<Integer>(Arrays.asList(Integer[].class.cast(obj)));
+			Assert.assertTrue(levels.contains(1),
+					"Conformance level 1 will not be checked as it is not included in ics");
+		}
+		super.obtainTestSubject(testContext);
 	}
 
 	/**
