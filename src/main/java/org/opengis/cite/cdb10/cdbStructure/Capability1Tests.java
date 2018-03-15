f676e8333ceb92f0bc07eab7fc8f5823020783a2
James Badger <james@jamesbadger.ca>
2018-03-15 11:07:01 -0600

    Check longitude geocells are validly named
    Does not check for geocells being variable-width - to be added

--- src/main/java/org/opengis/cite/cdb10/cdbStructure/Capability1Tests.java
+++ src/main/java/org/opengis/cite/cdb10/cdbStructure/Capability1Tests.java
@@ -153,4 +153,52 @@ public class Capability1Tests extends CommonFixture {
 
 		Assert.assertTrue(errors.size() == 0, StringUtils.join(errors, "\n"));
 	}
+
+	/**
+	 * Validates that longitude geocell directories end with a valid slice longitude.
+	 * longitudes should be zero-padded to 3 width.
+	 * (See volume 1, section 3.6.2.1).
+	 *
+	 * @throws IOException
+	 */
+	@Test
+	public void verifyGeocellLongitudeDirNameSlice() throws IOException {
+		ArrayList<String> errors = new ArrayList<String>();
+		DirectoryStream<Path> latitudeCells = Files.newDirectoryStream(Paths.get(this.path, "Tiles"));
+
+		for (Path latCell : latitudeCells) {
+			DirectoryStream<Path> longitudeCells = Files.newDirectoryStream(latCell);
+
+			for (Path lonCell : longitudeCells) {
+				String filename = lonCell.getFileName().toString();
+				String slice = filename.substring(1, filename.length());
+
+				if (filename.substring(0, 1).equals("E")) {
+					Integer sliceID = Integer.parseInt(slice);
+					if ((sliceID > 179) || (sliceID < 0)) {
+						errors.add("Invalid longitude for geocell directory name: " + filename);
+					}
+
+					if ((sliceID < 10) && (sliceID >= 0) && (slice.substring(1,3).equals("00"))) {
+						errors.add("Invalid zero-pad on geocell directory name: " + filename);
+					} else if ((sliceID < 100) && (sliceID >= 10) && (slice.substring(1,3).equals("0"))) {
+						errors.add("Invalid zero-pad on geocell directory name: " + filename);
+					}
+				} else if (filename.substring(0, 1).equals("W")) {
+					Integer sliceID = Integer.parseInt(slice);
+					if ((sliceID > 180) || (sliceID <= 0)) {
+						errors.add("Invalid longitude for geocell directory name: " + filename);
+					}
+
+					if ((sliceID < 10) && (sliceID >= 0) && (slice.substring(1,3).equals("00"))) {
+						errors.add("Invalid zero-pad on geocell directory name: " + filename);
+					} else if ((sliceID < 100) && (sliceID >= 10) && (slice.substring(1,3).equals("0"))) {
+						errors.add("Invalid zero-pad on geocell directory name: " + filename);
+					}
+				}
+			}
+		}
+
+		Assert.assertTrue(errors.size() == 0, StringUtils.join(errors, "\n"));
+	}
 }
