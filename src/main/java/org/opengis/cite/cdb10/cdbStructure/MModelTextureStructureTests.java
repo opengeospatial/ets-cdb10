70597d1ac33e41fcfea42d98fbe8fb3464f23c88
James Badger <james@jamesbadger.ca>
2018-03-16 14:33:43 -0600

    Test MModelTexture prefix directory format

--- src/main/java/org/opengis/cite/cdb10/cdbStructure/MModelTextureStructureTests.java
+++ src/main/java/org/opengis/cite/cdb10/cdbStructure/MModelTextureStructureTests.java
@@ -1,7 +1,39 @@
 package org.opengis.cite.cdb10.cdbStructure;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.StringUtils;
 import org.opengis.cite.cdb10.CommonFixture;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class MModelTextureStructureTests extends CommonFixture {
+	/**
+	 * Validates that MModelTexture Texture Name Prefix directories have valid codes.
+	 *
+	 * @throws IOException
+	 */
+	@Test
+	public void verifyTNAMPrefix() throws IOException {
+		ArrayList<String> errors = new ArrayList<String>();
+
+		for (Path tnamPrefixDir : Files.newDirectoryStream(Paths.get(this.path, "MModel", "601_MModelTexture"))) {
+			String filename = tnamPrefixDir.getFileName().toString();
+
+			if (filename.length() != 1) {
+				errors.add("Invalid length on texture name prefix directory: " + filename);
+			}
+
+			if (!filename.toUpperCase().equals(filename)) {
+				errors.add("Texture name prefix directory should be uppercase: " + filename);
+			}
+		}
+
+		Assert.assertTrue(errors.size() == 0, StringUtils.join(errors, "\n"));
+	}
 
 }
--- src/test/java/org/opengis/cite/cdb10/cdbStructure/VerifyMModelTextureStructureTests.java
+++ src/test/java/org/opengis/cite/cdb10/cdbStructure/VerifyMModelTextureStructureTests.java
@@ -1,11 +1,46 @@
 package org.opengis.cite.cdb10.cdbStructure;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Test;
 import org.opengis.cite.cdb10.TestFixture;
 
 public class VerifyMModelTextureStructureTests extends TestFixture<MModelTextureStructureTests> {
 
 	public VerifyMModelTextureStructureTests() {
 		this.testSuite = new MModelTextureStructureTests();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyTNAMPrefix_TooLong() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "601_MModelTexture",
+				"ABC")));
+
+		// execute
+		this.testSuite.verifyTNAMPrefix();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyTNAMPrefix_WrongCase() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "601_MModelTexture",
+				"a")));
+
+		// execute
+		this.testSuite.verifyTNAMPrefix();
+	}
+
+	@Test
+	public void verifyTNAMPrefix_Good() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "601_MModelTexture",
+				"A")));
+
+		// execute
+		this.testSuite.verifyTNAMPrefix();
 	}
 
 }
