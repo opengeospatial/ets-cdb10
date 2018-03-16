dd1ccb670227ec3fabeea74142d71723fa5c78e2
James Badger <james@jamesbadger.ca>
2018-03-16 15:17:22 -0600

    Test MModelSignature Entity Kind directories

--- src/main/java/org/opengis/cite/cdb10/cdbStructure/MModelSignatureStructureTests.java
+++ src/main/java/org/opengis/cite/cdb10/cdbStructure/MModelSignatureStructureTests.java
@@ -1,7 +1,58 @@
 package org.opengis.cite.cdb10.cdbStructure;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.StringUtils;
 import org.opengis.cite.cdb10.CommonFixture;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class MModelSignatureStructureTests extends CommonFixture {
+	/**
+	 * Validates that MModelSignature DIS Entity Kind directories have valid codes/names.
+	 *
+	 * @throws IOException
+	 */
+	@Test
+	public void verifyDISEntityKind() throws IOException {
+		ArrayList<String> errors = new ArrayList<String>();
+		MovingModelCodesXml mmcDefs = new MovingModelCodesXml("src/test/resources/CDB");
+
+		for (Path kindDir : Files.newDirectoryStream(Paths.get(this.path, "MModel", "606_MModelSignature"))) {
+			String filename = kindDir.getFileName().toString();
+			String code = null;
+			Integer codeID = null;
+			String kindName = null;
+			try {
+				code = filename.split("_")[0];
+				codeID = Integer.parseInt(code);
+				kindName = filename.split("_")[1];
+			}
+			catch (NumberFormatException e) {
+				errors.add("Invalid number format: " + filename);
+			}
+			catch (ArrayIndexOutOfBoundsException e) {
+				errors.add("Missing kind name: " + filename);
+			}
+
+			if (codeID != null) {
+				if (codeID < 0) {
+					errors.add("Invalid prefix cannot be below 0: " + filename);
+				} else if (!mmcDefs.isValidKindCode(codeID)) {
+					errors.add("Invalid DIS Entity Kind code: " + filename);
+				} else if (!mmcDefs.isValidKindName(kindName)) {
+					errors.add("Invalid DIS Entity Kind name: " + filename);
+				} else if (!mmcDefs.kindNameForCode(codeID).equals(kindName)) {
+					errors.add("Invalid DIS Entity Kind code/name combination: " + filename);
+				}
+			}
+		}
+
+		Assert.assertTrue(errors.size() == 0, StringUtils.join(errors, "\n"));
+	}
 
 }
--- src/test/java/org/opengis/cite/cdb10/cdbStructure/VerifyMModelSignatureStructureTests.java
+++ src/test/java/org/opengis/cite/cdb10/cdbStructure/VerifyMModelSignatureStructureTests.java
@@ -1,11 +1,66 @@
 package org.opengis.cite.cdb10.cdbStructure;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Test;
 import org.opengis.cite.cdb10.TestFixture;
 
 public class VerifyMModelSignatureStructureTests extends TestFixture<MModelSignatureStructureTests> {
 
 	public VerifyMModelSignatureStructureTests() {
 		this.testSuite = new MModelSignatureStructureTests();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyDISEntityKind_InvalidCode() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "606_MModelSignature",
+				"999_Kind")));
+
+		// execute
+		this.testSuite.verifyDISEntityKind();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyDISEntityKind_InvalidName() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "606_MModelSignature",
+				"1_Bogus")));
+
+		// execute
+		this.testSuite.verifyDISEntityKind();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyDISEntityKind_Mismatch() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "606_MModelSignature",
+				"1_Other")));
+
+		// execute
+		this.testSuite.verifyDISEntityKind();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyDISEntityKind_InvalidFile() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "606_MModelSignature",
+				"asdf")));
+
+		// execute
+		this.testSuite.verifyDISEntityKind();
+	}
+
+	@Test
+	public void verifyDISEntityKind_Good() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "606_MModelSignature",
+				"1_Platform")));
+
+		// execute
+		this.testSuite.verifyDISEntityKind();
 	}
 
 }
