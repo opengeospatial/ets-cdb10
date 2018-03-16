2b70aa1682f1709a9249c3755e3e6c47e0e697fd
James Badger <james@jamesbadger.ca>
2018-03-15 23:39:47 -0600

    Test MModel dataset directory format

---
+++ src/main/java/org/opengis/cite/cdb10/cdbStructure/MModelStructureTests.java
@@ -0,0 +1,60 @@
+package org.opengis.cite.cdb10.cdbStructure;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.StringUtils;
+import org.opengis.cite.cdb10.CommonFixture;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MModelStructureTests extends CommonFixture {
+	/**
+	 * Validates that MModel directories have valid codes/names.
+	 *
+	 * @throws IOException
+	 */
+	@Test
+	public void verifyDataset() throws IOException {
+		ArrayList<String> errors = new ArrayList<String>();
+		DatasetsXml datasetDefs = new DatasetsXml("src/test/resources/CDB");
+
+		for (Path file : Files.newDirectoryStream(Paths.get(this.path, "MModel"))) {
+			String filename = file.getFileName().toString();
+			String prefix = null;
+			Integer prefixID = null;
+			String datasetName = null;
+			try {
+				prefix = filename.substring(0, 3);
+				prefixID = Integer.parseInt(prefix);
+				datasetName = filename.split("_")[1];
+			}
+			catch (StringIndexOutOfBoundsException e) {
+				errors.add("Invalid prefix length: " + filename);
+			}
+			catch (NumberFormatException e) {
+				errors.add("Invalid number format: " + filename);
+			}
+			catch (ArrayIndexOutOfBoundsException e) {
+				errors.add("Missing dataset name: " + filename);
+			}
+
+			if (prefixID != null) {
+				if (prefixID < 1) {
+					errors.add("Invalid prefix cannot be below 001: " + filename);
+				} else if (!datasetDefs.isValidCode(prefixID)) {
+					errors.add("Invalid dataset code: " + filename);
+				} else if (!datasetDefs.isValidName(datasetName)) {
+					errors.add("Invalid dataset name: " + filename);
+				} else if (!datasetDefs.datasetNameForCode(prefixID).equals(datasetName)) {
+					errors.add("Invalid dataset code/name combination: " + filename);
+				}
+			}
+		}
+
+		Assert.assertTrue(errors.size() == 0, StringUtils.join(errors, "\n"));
+	}
+}
---
+++ src/test/java/org/opengis/cite/cdb10/cdbStructure/VerifyMModelStructureTests.java
@@ -0,0 +1,61 @@
+package org.opengis.cite.cdb10.cdbStructure;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+import org.opengis.cite.cdb10.TestFixture;
+
+public class VerifyMModelStructureTests extends TestFixture<MModelStructureTests> {
+
+	public VerifyMModelStructureTests() {
+		this.testSuite = new MModelStructureTests();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyDataset_BadShort() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "6_MModelGeometry")));
+
+		// execute
+		this.testSuite.verifyDataset();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyDataset_BadNumber() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "6xx_MModelGeometry")));
+
+		// execute
+		this.testSuite.verifyDataset();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyDataset_BadRange() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "-1_MModelGeometry")));
+
+		// execute
+		this.testSuite.verifyDataset();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyDataset_BadValue() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "999_MModelGeometry")));
+
+		// execute
+		this.testSuite.verifyDataset();
+	}
+
+	@Test
+	public void verifyDataset_Good() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("MModel", "600_MModelGeometry")));
+
+		// execute
+		this.testSuite.verifyDataset();
+	}
+
+}
