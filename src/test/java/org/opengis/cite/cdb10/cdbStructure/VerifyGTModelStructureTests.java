cfbabf981c2d3242b76c3e2ad0c4c2f696cc5dec
James Badger <james@jamesbadger.ca>
2018-12-14 10:52:01 -0700

    Add tests for GTModelGeometry LoD files

--- src/test/java/org/opengis/cite/cdb10/cdbStructure/VerifyGTModelStructureTests.java
+++ src/test/java/org/opengis/cite/cdb10/cdbStructure/VerifyGTModelStructureTests.java
@@ -434,4 +434,147 @@ public class VerifyGTModelStructureTests extends StructureTestFixture<GTModelStr
 		// execute
 		this.testSuite.verifyGeometryEntryFile();
 	}
+	
+	/*
+	 * GTModelGeometry LoD Naming
+	 */
+	
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_invalid() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"asdf")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_invalidDataset() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D501_S001_T001_L10_12345_001_amodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_invalidExtension() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_S001_T001_L10_12345_001_amodelname.txt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_badCS1Length() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_S1_T001_L10_12345_001_amodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_badCS1Type() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_SABC_T001_L10_12345_001_amodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_badCS2Length() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_S001_T1_L10_12345_001_amodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_badCS2Type() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_S001_TABC_L10_12345_001_amodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_badFCLength() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_S001_T001_L10_45_001_amodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_badFSCLength() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_S001_T001_L10_12345_1_amodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_badFSCType() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_S001_T001_L10_12345_ABC_amodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void verifyGeometryLoDFile_badMODLLength() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_S001_T001_L10_12345_001_amodelnameamodelnameamodelnameamodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+
+	@Test
+	public void verifyGeometryLoDFile_Good() throws IOException {
+		// setup
+		Files.createDirectories(this.cdb_root.resolve(Paths.get("GTModel", "510_GTModelGeometry",
+				"A_Culture", "A_Extraction", "010_Mine", "L10",
+				"D510_S001_T001_L10_12345_001_amodelname.flt")));
+
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
+
+	@Test
+	public void verifyGeometryLoDFile_Skip() throws IOException {
+		// setup
+		// execute
+		this.testSuite.verifyGeometryLoDFile();
+	}
 }
