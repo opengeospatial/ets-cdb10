a20d470be398c6858236f6feefe40f0fb2765754
James Badger <james@jamesbadger.ca>
2019-04-10 16:43:02 -0600

    Add package for tiled dataset tests
    These tile datasets are separated from the other datasets, and
    this package will have datasets re-using a common base test style.

---
+++ src/main/java/org/opengis/cite/cdb10/cdbStructure/tiledDatasets/ElevationTests.java
@@ -0,0 +1,13 @@
+package org.opengis.cite.cdb10.cdbStructure.tiledDatasets;
+import java.io.IOException;
+
+import org.opengis.cite.cdb10.CommonFixture;
+import org.testng.annotations.Test;
+
+public class ElevationTests extends CommonFixture {
+	
+	@Test
+	public void verifyDummy() throws IOException {
+		// Sample method
+	}
+}
---
+++ src/main/java/org/opengis/cite/cdb10/cdbStructure/tiledDatasets/package-info.java
@@ -0,0 +1,5 @@
+package org.opengis.cite.cdb10.cdbStructure.tiledDatasets;
+
+/*
+ * See Section 3.6, Volume 1, OGC CDB Core Standard (Version 1.0)
+*/
