878ef75d0b93e35fff9442852c4f3ad2b6abcf85
James Badger <james@jamesbadger.ca>
2019-04-11 11:21:39 -0600

    Remove unused test code
    Some of these test utility classes are not used, so they have
    been removed. The classes/methods may have been boilerplate.
    
    Sample XML files and schema files have been removed and will be
    replaced with CDB reference files.

--- src/test/java/org/opengis/cite/cdb10/util/VerifyValidationUtils.java
+++ src/test/java/org/opengis/cite/cdb10/util/VerifyValidationUtils.java
@@ -2,14 +2,6 @@ package org.opengis.cite.cdb10.util;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URI;
-import java.util.Set;
-
-import javax.xml.stream.XMLStreamException;
-import javax.xml.transform.stream.StreamSource;
-
 import org.junit.Test;
 import org.opengis.cite.validation.SchematronValidator;
 
@@ -28,16 +20,5 @@ public class VerifyValidationUtils {
         SchematronValidator result = ValidationUtils.buildSchematronValidator(
                 schemaRef, phase);
         assertNotNull(result);
-    }
-
-    @Test
-    public void extractRelativeSchemaReference() throws FileNotFoundException,
-            XMLStreamException {
-        File xmlFile = new File("src/test/resources/Alpha-1.xml");
-        Set<URI> xsdSet = ValidationUtils.extractSchemaReferences(
-                new StreamSource(xmlFile), null);
-        URI schemaURI = xsdSet.iterator().next();
-        assertTrue("Expected schema reference */xsd/alpha.xsd", schemaURI
-                .toString().endsWith("/xsd/alpha.xsd"));
     }
 }
