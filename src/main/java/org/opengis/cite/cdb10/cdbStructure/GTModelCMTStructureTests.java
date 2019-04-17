f2554275715a5187461bc6d2fe401c1cccae9ed6
James Badger <james@jamesbadger.ca>
2019-04-17 15:27:02 -0600

    Extract validation from test method
    This is an attempt to decouple the test validations from the for-loop
    soup that makes it messy to read.

--- src/main/java/org/opengis/cite/cdb10/cdbStructure/GTModelCMTStructureTests.java
+++ src/main/java/org/opengis/cite/cdb10/cdbStructure/GTModelCMTStructureTests.java
@@ -14,13 +14,13 @@ import org.opengis.cite.cdb10.CommonFixture;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GTModelCMTStructureTests extends CommonFixture {
+public class GTModelCMTStructureTests extends Capability1Tests {
 
 	/**
 	 * Validates that GTModelCMT filenames have valid codes/names.
 	 * Test based on Section 3.4.2, Volume 1, OGC CDB Core Standard (Version 1.0)
 	 *
-	 * @throws IOException
+	 * @throws IOException DirectoryStream error
 	 */
 	@Test
 	public void verifyCMTFile() throws IOException {
@@ -31,91 +31,92 @@ public class GTModelCMTStructureTests extends CommonFixture {
 		}
 
 		ArrayList<String> errors = new ArrayList<String>();
-		/*
-		 * Example of valid filename:
-		 * D505_S001_T001_AC_1.xml
-		 */
-		Pattern filePattern = Pattern.compile(
-				"^D505_S(?<cs1>\\d+)_T(?<cs2>\\d+)_(?<tnam>[^.]+)\\.(?<ext>.+)$"
-				);
 		
 		DirectoryStream<Path> tnamPrefixDirs = Files.newDirectoryStream(gtModelGeomPath);
 
 		for (Path tnamPrefixDir : tnamPrefixDirs) {
 			DirectoryStream<Path> secondDirs = Files.newDirectoryStream(tnamPrefixDir);
-			String firstDirFilename = tnamPrefixDir.getFileName().toString();
 
 			for (Path secondDir : secondDirs) {
 				DirectoryStream<Path> textureNames = Files.newDirectoryStream(secondDir);
-				String secondDirFilename = secondDir.getFileName().toString();
 
 				for (Path textureName : textureNames) {
 					DirectoryStream<Path> files = Files.newDirectoryStream(textureName);
-					String textureFilename = textureName.getFileName().toString();
 
 					for (Path file : files) {
-						String filename = file.getFileName().toString();
-
-						if (StringUtils.countMatches(filename, "_") != 4) {
-							errors.add("Should be four underscore separators: " + filename);
-						} else {
-							Matcher match = filePattern.matcher(filename);
-							if (!match.find()) {
-								errors.add("Invalid file name: " + filename);
-							} else {
-								if (match.group("cs1").length() != 3) {
-									errors.add("Component Selector 1 should be 3 characters: " + filename);
-								}
-	
-								try {
-									Integer cs1 = Integer.parseInt(match.group("cs1"));
-	
-									if (((cs1 < 10) && !match.group("cs1").substring(0,2).equals("00")) ||
-											((cs1 < 100) && !match.group("cs1").substring(0,1).equals("0"))) {
-										errors.add("Invalid padding on CS1: " + filename);
-									}
-								}
-								catch (NumberFormatException e) {
-									errors.add("Invalid CS1 number format: " + filename);
-								}
-								catch (StringIndexOutOfBoundsException e) {
-									errors.add("Invalid CS1 length: " + filename);
-								}
-	
-								if (match.group("cs2").length() != 3) {
-									errors.add("Component Selector 2 should be 3 characters: " + filename);
-								}
-	
-								try {
-									Integer cs2 = Integer.parseInt(match.group("cs2"));
-	
-									if (((cs2 < 10) && !match.group("cs2").substring(0,2).equals("00")) ||
-											((cs2 < 100) && !match.group("cs2").substring(0,1).equals("0"))) {
-										errors.add("Invalid padding on CS2: " + filename);
-									}
-								}
-								catch (NumberFormatException e) {
-									errors.add("Invalid CS2 number format: " + filename);
-								}
-								catch (StringIndexOutOfBoundsException e) {
-									errors.add("Invalid CS2 length: " + filename);
-								}
-								
-								if (!match.group("tnam").equals(textureFilename)) {
-									errors.add("Texture Name Code does not match parent directory: "
-											+ filename);
-								}
-								
-								if (!match.group("ext").equals("xml")) {
-									errors.add("Invalid file extension for D505: " + filename);
-								}
-							}
-						}
+						validateCMTFile(file, errors);
 					}
 				}
 			}
 		}
-
+		
 		Assert.assertTrue(errors.size() == 0, StringUtils.join(errors, "\n"));
+	}
+	
+	private void validateCMTFile(Path file, ArrayList<String> errors) {
+		/*
+		 * Example of valid filename:
+		 * D505_S001_T001_AC_1.xml
+		 */
+		final Pattern cmtFilePattern = Pattern.compile(
+				"^D505_S(?<cs1>\\d+)_T(?<cs2>\\d+)_(?<tnam>[^.]+)\\.(?<ext>.+)$");
+		
+		String filename = file.getFileName().toString();
+		Path textureName = file.getParent();
+		String textureFilename = textureName.getFileName().toString();
+		Matcher match = cmtFilePattern.matcher(filename);
+
+		if (StringUtils.countMatches(filename, "_") != 4) {
+			errors.add("Should be four underscore separators: " + filename);
+		} else if (!match.find()) {
+			errors.add("Invalid file name: " + filename);
+		} else {
+			if (match.group("cs1").length() != 3) {
+				errors.add("Component Selector 1 should be 3 characters: " + filename);
+			}
+
+			try {
+				Integer cs1 = Integer.parseInt(match.group("cs1"));
+
+				if (((cs1 < 10) && !match.group("cs1").substring(0,2).equals("00")) ||
+						((cs1 < 100) && !match.group("cs1").substring(0,1).equals("0"))) {
+					errors.add("Invalid padding on CS1: " + filename);
+				}
+			}
+			catch (NumberFormatException e) {
+				errors.add("Invalid CS1 number format: " + filename);
+			}
+			catch (StringIndexOutOfBoundsException e) {
+				errors.add("Invalid CS1 length: " + filename);
+			}
+
+			if (match.group("cs2").length() != 3) {
+				errors.add("Component Selector 2 should be 3 characters: " + filename);
+			}
+
+			try {
+				Integer cs2 = Integer.parseInt(match.group("cs2"));
+
+				if (((cs2 < 10) && !match.group("cs2").substring(0,2).equals("00")) ||
+						((cs2 < 100) && !match.group("cs2").substring(0,1).equals("0"))) {
+					errors.add("Invalid padding on CS2: " + filename);
+				}
+			}
+			catch (NumberFormatException e) {
+				errors.add("Invalid CS2 number format: " + filename);
+			}
+			catch (StringIndexOutOfBoundsException e) {
+				errors.add("Invalid CS2 length: " + filename);
+			}
+
+			if (!match.group("tnam").equals(textureFilename)) {
+				errors.add("Texture Name Code does not match parent directory: "
+						+ filename);
+			}
+
+			if (!match.group("ext").equals("xml")) {
+				errors.add("Invalid file extension for D505: " + filename);
+			}
+		}
 	}
 }
