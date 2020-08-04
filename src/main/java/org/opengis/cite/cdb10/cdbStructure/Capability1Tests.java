ef7e44f979146921c5981676d6d2adc4888c3125
James Badger <james@jamesbadger.ca>
2020-08-04 16:55:47 -0600

    Remove obsolete XML file reader
    This class isn't used anymore.

--- src/main/java/org/opengis/cite/cdb10/cdbStructure/Capability1Tests.java
+++ src/main/java/org/opengis/cite/cdb10/cdbStructure/Capability1Tests.java
@@ -2,7 +2,6 @@ package org.opengis.cite.cdb10.cdbStructure;
 
 import org.opengis.cite.cdb10.CommonFixture;
 import org.opengis.cite.cdb10.SuiteAttribute;
-import org.opengis.cite.cdb10.util.metadataXml.ComponentSelectorsXml;
 import org.opengis.cite.cdb10.util.metadataXml.DISCountryCodesXml;
 import org.opengis.cite.cdb10.util.metadataXml.MovingModelCodesXml;
 import org.opengis.cite.cdb10.util.reference.CdbReference;
