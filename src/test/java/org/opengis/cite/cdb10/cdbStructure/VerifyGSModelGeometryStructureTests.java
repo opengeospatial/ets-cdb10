c7261fb3efb980e3756e8eb3593e5de038760c24
James Badger <james@jamesbadger.ca>
2020-09-17 17:36:19 -0600

    Use empty ZIP for testing
    This is copied into a temporary directory to simulate various
    CDB misconfigurations, which in turn should raise errors.

--- src/test/java/org/opengis/cite/cdb10/cdbStructure/VerifyGSModelGeometryStructureTests.java
+++ src/test/java/org/opengis/cite/cdb10/cdbStructure/VerifyGSModelGeometryStructureTests.java
@@ -1,16 +1,28 @@
 package org.opengis.cite.cdb10.cdbStructure;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.opengis.cite.cdb10.cdbStructure.GSModelGeometryStructureTests;
 
 public class VerifyGSModelGeometryStructureTests extends StructureTestFixture<GSModelGeometryStructureTests> {
 	
+	protected final static Path SOURCE_DIRECTORY = Paths.get(System.getProperty("user.dir"), "src", "test", "java", "org", "opengis", "cite", "cdb10", "fixtures");
+	private final static Path EMPTY_ZIP = SOURCE_DIRECTORY.resolve(Paths.get("valid", "empty.zip"));
+	
 	public VerifyGSModelGeometryStructureTests() throws IOException {
 		this.testSuite = new GSModelGeometryStructureTests();
 	}
+	
+	@Before
+    public void setupDirs() throws IOException {
+    	Files.createDirectories(this.cdb_root.resolve(Paths.get("Tiles", "300_GSModelGeometry")));
+    }
 
 	/*
 	 * Verify GS Model Geometry filenames
@@ -18,9 +30,25 @@ public class VerifyGSModelGeometryStructureTests extends StructureTestFixture<GS
 	@Test
 	public void verifyGSModelFile_valid() throws IOException {
 		// setup
-		Files.createDirectories(this.cdb_root.resolve(
+		Path archive = this.cdb_root.resolve(
 				Paths.get("Tiles", "300_GSModelGeometry",
-						"N62W162_D300_S001_T001_L07_U38_R102.zip")));
+						"N62W162_D300_S001_T001_L07_U38_R102.zip"));
+		Files.copy(EMPTY_ZIP, archive);
+
+		// execute
+		this.testSuite.verifyGSModelFile();
+	}
+	
+	@Test
+	public void verifyGSModelFile_invalid() throws IOException {
+		// setup
+		Path archive = this.cdb_root.resolve(
+				Paths.get("Tiles", "300_GSModelGeometry",
+						"0.zip"));
+		Files.copy(EMPTY_ZIP, archive, REPLACE_EXISTING);
+		
+		expectedException.expect(AssertionError.class);
+        expectedException.expectMessage("Invalid file name");
 
 		// execute
 		this.testSuite.verifyGSModelFile();
