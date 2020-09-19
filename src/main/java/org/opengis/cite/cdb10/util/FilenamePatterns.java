ea7b791a2fd679e4d0f7ef08fce8b70712bee525
James Badger <james@jamesbadger.ca>
2020-09-18 19:43:18 -0600

    Add GSModelInteriorMaterialStructureTests

--- src/main/java/org/opengis/cite/cdb10/util/FilenamePatterns.java
+++ src/main/java/org/opengis/cite/cdb10/util/FilenamePatterns.java
@@ -46,6 +46,16 @@ public class FilenamePatterns {
 	public static final String GSModelInteriorGeometryEntry = "^(?<lat>(S|N)[0-9]{2})(?<lon>(E|W)[0-9]{3})_D(?<datasetCode>[0-9]{3})_S(?<cs1>[0-9]{3})_T(?<cs2>[0-9]{3})_(?<lod>LC\\d{2}|L[0-9]{2})_(?<uref>U[0-9]+)_(?<rref>R[0-9]+)_(?<featureCode>.{5})_(?<fsc>\\d+)_(?<modl>[^.]+)\\.(?<ext>.+)$";
 	
 	/**
+	 * Example of valid filename: N62W162_D308_S001_T001_L07_U38_R102.zip
+	 */
+	public static final String GSModelInteriorMaterial = GSModelArchive;
+	
+	/**
+	 * Example of valid filename: N62W162_D308_S001_T001_L07_U38_R102_AcmeFactory.tif
+	 */
+	public static final String GSModelInteriorMaterialEntry = "^(?<lat>(S|N)[0-9]{2})(?<lon>(E|W)[0-9]{3})_D(?<datasetCode>[0-9]{3})_S(?<cs1>[0-9]{3})_T(?<cs2>[0-9]{3})_(?<lod>LC\\d{2}|L[0-9]{2})_(?<uref>U[0-9]+)_(?<rref>R[0-9]+)_(?<tnam>[^.]+)\\.(?<ext>.+)$";
+	
+	/**
 	 * Example of valid filename: N62W162_D306_S001_T001_L07_U38_R102.zip
 	 */
 	public static final String GSModelInteriorTexture = GSModelArchive;
