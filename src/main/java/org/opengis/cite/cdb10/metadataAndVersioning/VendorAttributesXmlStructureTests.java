0edee8b1caae10a7a1311a75e4731cb20935fcda
James Badger <james@jamesbadger.ca>
2020-10-02 14:47:44 -0600

    Change Vendor Attributes tests to skip
    Revert this commit to re-add the TestNG validation tests for Vendor
    Attributes, and the JUnit self-tests.

--- src/main/java/org/opengis/cite/cdb10/metadataAndVersioning/VendorAttributesXmlStructureTests.java
+++ src/main/java/org/opengis/cite/cdb10/metadataAndVersioning/VendorAttributesXmlStructureTests.java
@@ -1,68 +1,33 @@
 package org.opengis.cite.cdb10.metadataAndVersioning;
 
-import java.io.IOException;
-
-import org.opengis.cite.cdb10.util.metadataXml.VendorAttributesXml;
-import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
-import org.xml.sax.SAXException;
 
 /**
  * Created by martin on 2016-09-09.
  */
 public class VendorAttributesXmlStructureTests extends Capability2Tests {
 	
-	private final static String NO_XML_SKIP = "Will not check for Vendor Attributes Schema file as no Vendor Attributes XML file exists.";
-	
-	private VendorAttributesXml vendorAttributes;
-
-	/**
-	 * Initialize the VendorAttributesXml validation class.
-	 */
-	private void loadXmlFile() {
-		this.vendorAttributes = new VendorAttributesXml(path);
-	}
-	
-	private Boolean xmlFileExists() {
-		return this.vendorAttributes.xmlFileExists();
-	}
+	private final static String SKIP_VENDOR_ATTRIBUTES = "The OGC CDB 1.0 standard is unclear about whether Vendor_Attributes.xsd and Geomatics_Attributes.xsd, or Vector_Attributes.xsd should be used. Therefore this test is skipped.";
 
 	/**
 	 * Verify that the schema file exists for a Vendor Attributes XML file.
 	 * If no Vendor Attributes XML file exists, then this test is skipped.
+	 * 
+	 * Due to ambiguities in OGC CDB Standard 1.0, this test is always skipped.
 	 */
     @Test(description = "OGC 15-113r5, Section 3.1.1")
-    public void verifyVendorAttributesSchemaExists() {
-        this.loadXmlFile();
-        // If there is no "Vendor_Attributes.xml", then skip
-        if (!this.xmlFileExists()) {
-        	throw new SkipException(NO_XML_SKIP);
-        }
-        
-		Assert.assertTrue(vendorAttributes.xsdFileExists(),
-				"Schema could not be loaded from XML 'schemaLocation'.");
+    public void verifyVendorAttributesSchemaExists() {        
+        throw new SkipException(SKIP_VENDOR_ATTRIBUTES);
     }
 
     /** If the Vendor Attributes XML and Schema files exist, then verify the
      * XML against the schema.
      * 
-     * @throws IOException Error reading XML or Schema file
-     * @throws SAXException Error parsing XML or Schema file
+	 * Due to ambiguities in OGC CDB Standard 1.0, this test is always skipped.
      */
     @Test(description = "OGC 15-113r5, A.1.19, Test 76")
-    public void verifyVendorAttributesXmlAgainstSchema() throws IOException, SAXException {
-        this.loadXmlFile();
-        // If there is no "Vendor_Attributes.xml", then skip
-        if (!this.xmlFileExists()) {
-        	throw new SkipException(NO_XML_SKIP);
-        }
-        
-        Assert.assertTrue(vendorAttributes.xsdFileExists(),
-				String.format("Metadata directory should contain %s file.", vendorAttributes.getXsdFileName()));
-        
-        String errors = vendorAttributes.schemaValidationErrors();
-        Assert.assertEquals(errors, "", vendorAttributes.getXmlFileName() + 
-        		" does not validate against its XML Schema file. Errors: " + errors);
+    public void verifyVendorAttributesXmlAgainstSchema() {
+    	throw new SkipException(SKIP_VENDOR_ATTRIBUTES);
     }
 }
--- src/test/java/org/opengis/cite/cdb10/metadataAndVersioning/VerifyVendorAttributesXmlStructureTests.java
+++ src/test/java/org/opengis/cite/cdb10/metadataAndVersioning/VerifyVendorAttributesXmlStructureTests.java
@@ -2,97 +2,18 @@ package org.opengis.cite.cdb10.metadataAndVersioning;
 
 import org.junit.Test;
 import org.testng.SkipException;
-import org.xml.sax.SAXException;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-
-/**
- * Created by martin on 2016-09-09.
- */
 public class VerifyVendorAttributesXmlStructureTests extends MetadataTestFixture<VendorAttributesXmlStructureTests> {
-	
-	private static final String VENDOR_ATTRIBUTES_XSD = "Vendor_Attributes.xsd";
-	private static final String VENDOR_ATTRIBUTES_XML = "Vendor_Attributes.xml";
     
-	private final static Path XSD_FILE = SOURCE_DIRECTORY.resolve(Paths.get("schema", VENDOR_ATTRIBUTES_XSD));
-    private final static Path XML_LOCAL_XSD = SOURCE_DIRECTORY.resolve(Paths.get("valid", VENDOR_ATTRIBUTES_XML));
-    private final static Path XML_REMOTE_XSD_MISSING = SOURCE_DIRECTORY.resolve(Paths.get("invalid", "Vendor_AttributesRemoteMissing.xml"));
-    private final static Path XML_INVALID = SOURCE_DIRECTORY.resolve(Paths.get("invalid", "Vendor_AttributesInvalid.xml"));
-
     public VerifyVendorAttributesXmlStructureTests() { testSuite = new VendorAttributesXmlStructureTests(); }
     
     @Test
-    public void verifyVendorAttributesSchemaExists_XmlFileDoesNotExist() {
+    public void verifyVendorAttributesSchemaExists_Skips() {
     	// setup: No Vendor_Attributes.xml
     	expectedException.expect(SkipException.class);
-        expectedException.expectMessage("Will not check for Vendor Attributes Schema file as no Vendor Attributes XML file exists.");
+        expectedException.expectMessage("The OGC CDB 1.0 standard is unclear about whether Vendor_Attributes.xsd and Geomatics_Attributes.xsd, or Vector_Attributes.xsd should be used. Therefore this test is skipped.");
         
     	// execute
     	testSuite.verifyVendorAttributesSchemaExists();
-    }
-
-    @Test
-    public void verifyVendorAttributesSchemaExists_XsdDoesNotExist() throws IOException {
-        // setup
-        Files.copy(XML_LOCAL_XSD, metadataFolder.resolve(VENDOR_ATTRIBUTES_XML), REPLACE_EXISTING);
-        expectedException.expect(AssertionError.class);
-        expectedException.expectMessage("Schema could not be loaded from XML 'schemaLocation'.");
-
-        // execute
-        testSuite.verifyVendorAttributesSchemaExists();
-    }
-    
-    @Test
-    public void verifyVendorAttributesSchemaExists_RemoteXsdDoesNotExist() throws IOException {
-        // setup
-        Files.copy(XML_REMOTE_XSD_MISSING, metadataFolder.resolve(VENDOR_ATTRIBUTES_XML), REPLACE_EXISTING);
-        expectedException.expect(AssertionError.class);
-        expectedException.expectMessage("Schema could not be loaded from XML 'schemaLocation'.");
-
-        // execute
-        testSuite.verifyVendorAttributesSchemaExists();
-    }
-    
-    @Test
-    public void verifyVendorAttributesSchemaExists_XsdDoesExist() throws IOException {
-        // setup
-        Files.copy(XML_LOCAL_XSD, metadataFolder.resolve(VENDOR_ATTRIBUTES_XML), REPLACE_EXISTING);
-        Files.copy(XSD_FILE, schemaFolder.resolve(VENDOR_ATTRIBUTES_XSD), REPLACE_EXISTING);
-
-        // execute
-        testSuite.verifyVendorAttributesSchemaExists();
-    }
-
-    @Test
-    public void verifyVendorAttributesXmlAgainstSchema_XmlIsValid() throws IOException, SAXException {
-        // setup
-        Files.copy(XML_LOCAL_XSD, metadataFolder.resolve(VENDOR_ATTRIBUTES_XML), REPLACE_EXISTING);
-        Files.copy(XSD_FILE, schemaFolder.resolve(VENDOR_ATTRIBUTES_XSD), REPLACE_EXISTING);
-
-        // execute
-        testSuite.verifyVendorAttributesXmlAgainstSchema();
-    }
-
-    @Test
-    public void verifyVendorAttributesXmlAgainstSchema_XmlIsNotValid() throws IOException, SAXException {
-        // setup
-        Files.copy(XML_INVALID, metadataFolder.resolve(VENDOR_ATTRIBUTES_XML), REPLACE_EXISTING);
-        Files.copy(XSD_FILE, schemaFolder.resolve(VENDOR_ATTRIBUTES_XSD), REPLACE_EXISTING);
-
-        String expectedMessage = "Vendor_Attributes.xml does not validate against its XML Schema file. " +
-                "Errors: cvc-complex-type.2.4.b: The content of element 'Vendor_Attributes' is not complete. " +
-                "One of '{\"http://www.CDB-Spec.org/Schema/Version/3.2\":Example_Element_2}' is expected.";
-
-
-        expectedException.expect(AssertionError.class);
-        expectedException.expectMessage(expectedMessage);
-
-        // execute
-        testSuite.verifyVendorAttributesXmlAgainstSchema();
     }
 }
