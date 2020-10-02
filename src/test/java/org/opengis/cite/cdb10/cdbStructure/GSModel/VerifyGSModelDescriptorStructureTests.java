84a054a45a2b522644e699424d72e9d5dee099bf
James Badger <james@jamesbadger.ca>
2020-10-02 17:03:38 -0600

    Adjust GSModelDescriptor tests to correct path
    For example, instead of searching:
    	Tiles > 303_GSModelDescriptor > (archive)
    
    The test suite will search:
    	Tiles > 303_GSModelDescriptor > L07 > U38 > (archive)

--- src/test/java/org/opengis/cite/cdb10/cdbStructure/GSModel/VerifyGSModelDescriptorStructureTests.java
+++ src/test/java/org/opengis/cite/cdb10/cdbStructure/GSModel/VerifyGSModelDescriptorStructureTests.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class VerifyGSModelDescriptorStructureTests extends GSModelStructureTests<GSModelDescriptorStructureTests> {
@@ -18,26 +17,29 @@ public class VerifyGSModelDescriptorStructureTests extends GSModelStructureTests
 	
 	protected static final String VALID_ARCHIVE_NAME = "N62W162_D303_S001_T001_L07_U38_R102.zip";
 	protected static final String VALID_ENTRY_NAME = "N62W162_D303_S001_T001_L07_U38_R102_AL015_116_AcmeFactory.xml";
+	protected static final String VALID_LOD = "L07";
+	protected static final String VALID_UREF = "U38";
 	
 	public VerifyGSModelDescriptorStructureTests() throws IOException {
 		this.testSuite = new GSModelDescriptorStructureTests();
 	}
-	
-	@Before
-    public void setupDirs() throws IOException {
-    	Files.createDirectories(this.cdb_root.resolve(Paths.get("Tiles", GSModelDescriptorStructureTests.DATASET_DIRECTORY)));
-    }
-	
+
 	/**
 	 * Creates a Path for a GSModelDescriptor archive with a custom filename.
 	 * Filename must include file extension. Archive will be placed in:
-	 * CDB Root > Tiles > 303_GSModelDescriptor
+	 * CDB Root > Tiles > 303_GSModelDescriptor > Lod > Uref
 	 * 
 	 * @param archiveFilename
 	 * @return Path for GSModelDescriptor archive file
+	 * @throws IOException 
 	 */
-	protected Path createGSModelDescriptorArchive(String archiveFilename) {
-		return this.cdb_root.resolve(Paths.get("Tiles", GSModelDescriptorStructureTests.DATASET_DIRECTORY, archiveFilename));
+	protected Path createGSModelDescriptorArchive(String archiveFilename) throws IOException {
+		Path parentDir = Paths.get("Tiles", 
+				GSModelDescriptorStructureTests.DATASET_DIRECTORY, VALID_LOD, VALID_UREF);
+		
+		Files.createDirectories(this.cdb_root.resolve(parentDir));
+		
+		return this.cdb_root.resolve(Paths.get(parentDir.toString(), archiveFilename));
 	}
 
 	/*
